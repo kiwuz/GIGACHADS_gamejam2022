902400bee9fcd24852e66199dfa2d98aa5c5b19c
ulewq <ulewinskil@gmail.com>
2022-06-17 23:53:56 +0200

    enigma added

---
+++ Assets/Leszek/MenuScripts.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScripts : MonoBehaviour
+{
+
+    [SerializeField]
+    private InputField inputField;
+    [SerializeField]
+    private Text Text;
+    [SerializeField]
+    private Canvas canvas;
+
+    public void EnigmaChecker()
+    {
+        if (inputField.text == "mein fuhrer")
+        {
+            Text.text = "Tu bedzie haslo";
+        }
+        else
+        {
+            Debug.Log("KURWA ZLE");
+        }
+    }
+
+    public void disableEnigma()
+    {
+        canvas.enabled = false;
+    }
+
+}
